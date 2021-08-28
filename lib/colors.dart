d08238cad6a348fb7ff905b2cb09287868a9ace8
avinashkatariya <44751905+avinashkatariya@users.noreply.github.com>
2021-08-28 14:30:25 +0530

    app only branch

--- lib/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color primary = Color(0xFFf6820c);
-Color textColor = Color(0xaa000000);
-Color textColorBold = Color(0xFF121212);
-Color primaryLight = Color(0x88f6820c);
-Color whiteText = Color(0xFFF9F9F9);
